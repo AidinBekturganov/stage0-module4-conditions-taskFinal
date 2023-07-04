3f63abff44d090c7cbcab10db698ba16e5c35cf7
Aidin Bekturganov <bekturganovaidin000@gmail.com>
2023-07-04 15:45:49 +0600

    finished task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,38 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (year > 0 && month > 0 && month < 13) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        if (month == 2) {
+                            System.out.println(29);
+                            return;
+                        }
+                        if (month == 6) {
+                            System.out.println(30);
+                            return;
+                        }
+                    }
+                } else {
+                    if (month == 2) {
+                        System.out.println(29);
+                        return;
+                    }
+                    if (month == 6) {
+                        System.out.println(30);
+                        return;
+                    }
+                }
+            }
+            if (month == 2) {
+                System.out.println(28);
+            }
+            if (month == 6) {
+                System.out.println(30);
+            }
+        } else {
+            System.out.println("invalid date");
+        }
     }
 }
